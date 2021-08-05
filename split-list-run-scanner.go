560a736494fde138a53e572f3abff60b85a27e1e
Maximilian Hildebrand <maximilian.hildebrand@posteo.de>
2021-08-05 14:18:24 +0200

    fixed powershell for windows

--- split-list-run-scanner.go
+++ split-list-run-scanner.go
@@ -17,6 +17,10 @@ var (
 	green  = "\033[32m"
 )
 
+/*
+go run .\split-list-run-scanner.go -list "C:\Users\max\Documents\git\Web-Cache-Vulnerability-Scanner\test\adobe\domains" -lines 500 -outputpath "C:\Users\max\Documents\git\Web-Cache-Vulnerability-Scanner\test\adobe\new"
+*/
+
 const replaceString = "$LIST"
 
 var sortedList []string
@@ -29,7 +33,7 @@ func main() {
 		green = ""
 	}
 
-	pathList, pathOutput, linesLimit, command := parseFlags()
+	pathList, pathOutput, nameOutput, linesLimit, command := parseFlags()
 
 	if pathList == "" {
 		fmt.Printf("%sError: -list wasn't specified%s\n", red, reset)
@@ -41,10 +45,10 @@ func main() {
 	} else {
 		pathOutput = strings.TrimSuffix(pathOutput, "/")
 		pathOutput = strings.TrimSuffix(pathOutput, "\\")
-		pathOutput += "/"
+		pathOutput += "\\"
 	}
 	if !strings.Contains(command, replaceString) {
-		fmt.Printf("%sWarning: -command doesn't contain %s%s", yellow, replaceString, reset)
+		fmt.Printf("%sWarning: -command doesn't contain %s%s\n", yellow, replaceString, reset)
 	}
 
 	sliceList := readLocalFile(pathList)
@@ -57,26 +61,31 @@ func main() {
 
 	for i, line := range sliceList {
 		if i%linesLimit == 0 {
-			path := fmt.Sprintf("%s%d/", pathOutput, i)
-			f, err = os.Create(path)
+			path := fmt.Sprintf("%s%d\\", pathOutput, i)
+			err = os.MkdirAll(path, 0755)
 			if err != nil {
-				msg := "Log: " + err.Error() + "\n"
-				fmt.Print(msg)
+				fmt.Printf("%sError MkDir:%s%s\n", red, err.Error(), reset)
+			}
+			f, err = os.Create(path + nameOutput)
+			if err != nil {
+				fmt.Printf("%sError CreateFile:%s%s\n", red, err.Error(), reset)
 			}
 			paths = append(paths, path)
 			defer f.Close()
 		}
-		f.WriteString(line + "\n")
+		f.WriteString(line)
+		if i%linesLimit != linesLimit-1 && i+1 < len(sliceList) {
+			f.WriteString("\n")
+		}
 	}
 
 	for _, p := range paths {
-		commandNew := strings.Replace(command, replaceString, p, -1)
-		cmd := exec.Command(commandNew)
-		err := cmd.Run()
+		commandNew := strings.Replace(command, replaceString, p+nameOutput, -1)
+		commandNew = strings.Replace(commandNew, "$PATH", p, -1)
+		fmt.Println(commandNew)
 
-		if err != nil {
-			fmt.Printf("%sError: %s%s\n", red, err, reset)
-		}
+		cmd := exec.Command("powershell", "start-process", "powershell.exe", "-argument", "'"+commandNew+"'")
+		cmd.Start()
 	}
 
 	fmt.Printf("%sFinished.%s", green, reset)
@@ -86,25 +95,27 @@ func readLocalFile(path string) []string {
 
 	w, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Printf("%s%s: %s%s\n", red, path, err.Error(), reset)
+		fmt.Printf("%sError while Reading list %s: %s%s\n", red, path, err.Error(), reset)
 		os.Exit(3)
 	}
 
 	return strings.Split(string(w), "\n")
 }
 
-func parseFlags() (string, string, int, string) {
+func parseFlags() (string, string, string, int, string) {
 	var pathList string
 	var pathOutput string
 	var lines int
 	var command string
+	var nameOutput string
 
 	flag.StringVar(&pathList, "list", "", "path to the list")
-	flag.StringVar(&pathOutput, "output", "", "path to output folder")
+	flag.StringVar(&pathOutput, "outputpath", "", "path to output folder")
+	flag.StringVar(&nameOutput, "outputname", "list", "name for output file. Default is 'list'")
 	flag.IntVar(&lines, "lines", 1000, "after how many lines should be splitted? Default is 1000")
-	flag.StringVar(&pathOutput, "command", "", "command to run. Use $LIST where the path of a splitted list shall be inserted")
+	flag.StringVar(&command, "command", "", "command to run. Use $LIST where the path of a splitted list shall be inserted")
 
 	flag.Parse()
 
-	return pathList, pathOutput, lines, command
+	return pathList, pathOutput, nameOutput, lines, command
 }
